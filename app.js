7bf4b5a62cf3738fdb1d6d4f4f1973c62aa98471
Marinela Togoe <marinelatogoe@gmail.com>
2022-06-15 17:40:59 +0200

    Tested request methods, as the default /Get

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => { 
-    console.log(req);
-    process.exit();
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
 });
 
 server.listen(3000);
