d321738b672efff5992e78db455292d4a416a4e9
Marinela Togoe <marinelatogoe@gmail.com>
2022-06-20 12:38:40 +0200

    Added the firts middleware inside app.js

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const express = require('express'); // third party package;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next(); // Allows the request to continue to the next middleware in line;
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
