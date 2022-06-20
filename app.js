f3eee6d891d508eedf1b2e4a377949525562f958
Marinela Togoe <marinelatogoe@gmail.com>
2022-06-20 13:32:00 +0200

    Installed npm package body-parser for parsing incoming requests

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const http = require('http'); // Node specific module
 
 const express = require('express'); // third party package;
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
   console.log('In a middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In a second middleware!');
   res.send('<h1>Hello form Express!</h1>');
 });
 
