648f177860e789752d54b6a6a7de166a132bbb66
Marinela Togoe <marinelatogoe@gmail.com>
2022-06-20 14:12:42 +0200

    Separated the code into two files inside the routes folder using the express.Router() function

--- app.js
+++ app.js
@@ -5,20 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin'); // Own file;
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In a middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello form Express!</h1>');
-});
 
 app.listen(3000);
