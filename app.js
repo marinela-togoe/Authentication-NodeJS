c17717cd2c6fbaca070ba788316c30d9434396b3
Marinela Togoe <marinelatogoe@gmail.com>
2022-06-20 12:20:30 +0200

    Installed ExpressJS

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const http = require('http');
-const routes = require('./routes');
+const http = require('http'); // Node specific module
 
-const server = http.createServer(routes);
+const express = require('express'); // third party package;
+
+
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
