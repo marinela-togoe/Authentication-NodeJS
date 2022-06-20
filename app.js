07ccd3b77816903105d1e1230db8ca9f44d182b5
Marinela Togoe <marinelatogoe@gmail.com>
2022-06-20 13:08:28 +0200

    Added template inside app.js for different routes

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const express = require('express'); // third party package;
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // Allows the request to continue to the next middleware in line;
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In a middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware!');
+app.use('/', (req, res, next) => {
+  console.log('In a second middleware!');
+  res.send('<h1>Hello form Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
