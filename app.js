a713fbfca508ab7e9637255d663027ad4ed707c3
Marinela Togoe <marinelatogoe@gmail.com>
2022-06-22 20:12:27 +0200

    Added ejs templating for all the files

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const http = require('http'); // Node specific module
-const path = require('path'); // Makes it work in all operating systems;
+const path = require('path');
 
-const express = require('express'); // third party package;
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin'); // Own file;
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-   res.status(404).render('404', {pageTitle: 'Page Not Found'}); 
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
