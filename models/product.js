0974ebffb84894d8756ce0ac13b098e224ce83a9
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-04 13:06:12 +0200

    Added functionality for fetching a single product

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 
@@ -36,6 +37,19 @@ class Product {
     });
   }
 
+    static findById(prodId) {
+      const db = getDb();
+      return db.collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
 }
 
 
