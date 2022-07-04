d1551fa50f896ee69ac3a95a887dadbbaf96048a
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-04 11:47:00 +0200

    Added template for the database connection

--- util/database.js
+++ util/database.js
@@ -1,17 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
 
 const mongoConnect = (callback) => {
 
-    MongoClient.connect('mongodb+srv://marinela:ObQTp8q68yAdP0qq@cluster0.udspa.mongodb.net/?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://marinela:ObQTp8q68yAdP0qq@cluster0.udspa.mongodb.net/shop?retryWrites=true&w=majority')
     .then(client => {
         console.log('Connected!');
-        callback(client);
+        _db = client.db()
+        callback();
     })
     .catch(err => {
         console.log(err);  
+        throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+   if (_db) {
+    return _db;
+   } 
+   throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
