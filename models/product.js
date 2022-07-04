d69f4003cbab68e94b607a4ffd0c9add63602913
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-04 16:22:18 +0200

    Added functionality to delete the product

--- models/product.js
+++ models/product.js
@@ -49,19 +49,32 @@ class Product {
     });
   }
 
-    static findById(prodId) {
-      const db = getDb();
-      return db.collection('products')
-      .find({ _id: new mongodb.ObjectId(prodId) })
-      .next()
-      .then(product => {
-        console.log(product);
-        return product;
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+    .find({ _id: new mongodb.ObjectId(prodId) })
+    .next()
+    .then(product => {
+      console.log(product);
+      return product;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+     return db
+    .collection('products')
+    .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+    .then(result => {
+      console.log('Deleted');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
 }
 
 
