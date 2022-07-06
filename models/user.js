aa45b89d153bc754ba59653bc899e8ca51949ecd
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-06 19:38:36 +0200

    Added functionality for getting orders

--- models/user.js
+++ models/user.js
@@ -111,7 +111,9 @@ class User {
 
   getOrders() {
     const db = getDb(); 
-    // return db.collection('orders')
+    return db.collection('orders')
+    .find({'user._id': new ObjectId(this._id)})
+    .toArray();
 
   }
 
