e453f0648ac15505255ebf8bf921182be8079264
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-08 17:58:25 +0200

    Added functionality for updating the data

--- controllers/admin.js
+++ controllers/admin.js
@@ -40,7 +40,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-    // Product.findById(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -62,16 +61,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc, 
-    updatedImageUrl,
-    prodId
-  );
+  Product.findById(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
 
-  product
-  .save()
+    return product.save();
+  })
   .then(result => {
     console.log('UPDATED PRODUCT!');
     res.redirect('/admin/products');
@@ -80,7 +78,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
