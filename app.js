1c641baccbb676d3aae5d2f931c737a7f49120d2
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-08 17:30:22 +0200

    Created functionality to save data through Mongoose

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,26 +18,33 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('62c330fd1d217b5307f53c81')
-    .then(user => {
-      req.user = new User(
-        user.name, 
-        user.email, 
-        user.cart, 
-        user._id
-      );
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('62c330fd1d217b5307f53c81')
+//     .then(user => {
+//       req.user = new User(
+//         user.name, 
+//         user.email, 
+//         user.cart, 
+//         user._id
+//       );
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
- 
+
+mongoose
+.connect(
+  'mongodb+srv://marinela:ObQTp8q68yAdP0qq@cluster0.udspa.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result => {
   app.listen(3000);
-});
+})
+.catch(err => {
+  console.log(err);
+})
+
