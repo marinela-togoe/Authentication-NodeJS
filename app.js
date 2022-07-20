47bb579d51bec83b346e61514f77378b802b40ee
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-20 14:30:14 +0200

    Added functionality for 'Add to Cart'

--- app.js
+++ app.js
@@ -39,15 +39,17 @@ app.use(
    })
 );
 
-
-// app.use((req, res, next) => {
-//   User.findById('62c85aaea17e7e6de249f7fe')
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
