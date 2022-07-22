47b82057bc3e9fb3e51a95f483c6238e1b7d5e78
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-22 13:13:56 +0200

    Added initial template for authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,12 +6,20 @@ exports.getLogin = (req, res, next) => {
     // .split(';')[1]
     // .trim()
     // .split('=')[1] === 'true';
-    console.log(req.session.isLoggedIn);
+    // console.log(req.session.isLoggedIn);
     res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     isAuthenticated: false
     });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
 };
 
 exports.postLogin = (req, res, next) => {
@@ -22,10 +30,12 @@ exports.postLogin = (req, res, next) => {
       req.session.save((err) => {
         console.log(err);
         res.redirect('/');
-      })
+      });
     })
     .catch(err => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy((err) => {
