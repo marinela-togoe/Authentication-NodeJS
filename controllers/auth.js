a4020ce3ca7fe78a3f5e6ed7b2c24d1d565844ff
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-27 16:10:21 +0200

    Installed Nodemailer and Nodemailer-Sendgrid-Transport npm packages to send confirmation email to the user upon signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -83,6 +91,15 @@ exports.postSignup = (req, res, next) => {
     })
     .then(result => {
       res.redirect('/login');
+      return transporter.sendMail({
+        to: email,
+        from: 'nodejs.testing1212@gmail.com',
+        subject: 'Signup succeeded!',
+        html: '<h1>You successfully signed up!</h1>'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
   })
   .catch(err => {
